9e91c815b378cdeb5dd5af87773a06b335331dbd
Junior-Santos-15 <128625004+Junior-Santos-15@users.noreply.github.com>
2023-05-30 14:17:03 -0300

    Add files via upload

---
+++ Par e impar.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+
+    int i, par = 0, impar = 0;
+    int num;
+
+    // Laço de repetição para -> if
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("Digite o valor:");
+        scanf("%d", &num);
+
+        if (num % 2 == 0)
+        {
+            par++;
+        }
+            else
+            {
+                impar++;
+            }
+    }
+
+    // Resolução de resposta
+
+    printf("Número par : %d\n", par);
+    printf("Número impar : %d\n", impar);
+
+    system("pause");
+
+    return 0;
+}
